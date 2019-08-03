bb6b5954328da692a6a4d5230b678f46e8bfb323
Marianna <mckd2018@gmail.com>
2019-08-03 11:13:49 -0400

    [add] getting weather description

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 """
 Plan
 - Create interface for user (text field + button)
-- Get user input
+- [x] Get user input
 - Display mood options to users
-- Get weather data from API
+- [x] Get weather data from API
     - Do a call 10 times for the number of moods
 - Associate weather data with moods
     Walk the json and add the value the mood
@@ -28,31 +28,30 @@ import configparser
 import requests
 import sys
 
-
+#locations = [London, Detroit, Lansing, Dallas, Los Angeles, San Francisco, Atlantia, Washingtion.DC, Boston, Baltimore]'''
+city_ids = [ 6058560, 4990729, 4998830, 4058076, 5368361, 5391959, 4180439, 2634716, 4317656, 4347778]
 def get_api_key():
     config = configparser.ConfigParser()
     config.read('config.ini')
     return config['openweathermap']['api']
 
-def get_weather(api_key, location):
-    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid={}".format(location, api_key)
+def get_weather(api_key, city):
+    """Return weather based on city ID"""
+    # for city in city_ids:
+    url = "https://api.openweathermap.org/data/2.5/forecast?id={}&appid={}".format(city, api_key)
     r = requests.get(url)
     return r.json()
 
 def main():
-    if len(sys.argv) != 2:
-        exit("Usage: {} LOCATION".format(sys.argv[0]))
-    location = sys.argv[1]
-    print(f"This is location: {location}")
-
+    # display message if location not given in terminal
+    # if len(sys.argv) != 2:
+    #     exit("Usage: {} LOCATION".format(sys.argv[0]))
+    location = 6058560 #sys.argv[1]
+    # gets data from API
     api_key = get_api_key()
     weather = get_weather(api_key, location)
-
-    print(weather['weather'][0]['description'])
-    # inner_weather = weather['weather']
-    # descriptive_weather = inner_weather['description']
-    print('\n')
-    print(weather)
+    # gets the description
+    print(weather['list'][10]['weather'][0]['description'])
 
 
 if __name__ == '__main__':
